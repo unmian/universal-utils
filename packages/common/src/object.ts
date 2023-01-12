eae8aef6c100689177b026dbab7488f38e85417d
Quarter <master@shiruihua.cn>
2023-01-12 11:37:30 +0800

    feat: 新增对象深拷贝方法

--- packages/common/src/object.ts
+++ packages/common/src/object.ts
@@ -2,8 +2,8 @@
  * @Author: Quarter
  * @Date: 2022-08-23 17:18:54
  * @LastEditors: Quarter
- * @LastEditTime: 2022-08-23 17:22:50
- * @FilePath: /universal-utils/src/object.ts
+ * @LastEditTime: 2023-01-12 11:33:00
+ * @FilePath: /universal-utils/packages/common/src/object.ts
  * @Description: 对象工具函数
  */
 
@@ -51,4 +51,34 @@ export const excludeProperties = (
     delete temp[properties];
   }
   return temp;
+};
+
+/**
+ * @description: 对象深拷贝
+ * @param {T} obj 对象
+ * @return {T}
+ */
+export const copy = <T>(obj: T): T => {
+  // 判断是否为对象
+  if (typeof obj !== "object" || obj === null) {
+    return obj;
+  }
+  if (obj === null) {
+    return null as unknown as typeof obj;
+  } else if (Array.isArray(obj)) {
+    // 实现array数据的深拷贝
+    return [...obj].map((item) => copy(item)) as unknown as typeof obj;
+  } else if (obj instanceof Set) {
+    // 实现set数据的深拷贝
+    return new Set([...obj]) as unknown as typeof obj;
+  } else if (obj instanceof Map) {
+    // 实现map数据的深拷贝
+    return new Map([...obj]) as unknown as typeof obj;
+  }
+  const result = {} as unknown as typeof obj;
+  Object.keys(obj).forEach((key) => {
+    const value = Reflect.get(obj, key);
+    Reflect.set(result, key, copy(value));
+  });
+  return result;
 };
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -2,7 +2,7 @@
  * @Author: Quarter
  * @Date: 2022-08-23 17:33:16
  * @LastEditors: Quarter
- * @LastEditTime: 2022-08-29 18:03:20
+ * @LastEditTime: 2023-01-12 11:33:50
  * @FilePath: /universal-utils/tests/object.test.ts
  * @Description: 对象工具测试用例
  */
@@ -49,5 +49,73 @@ describe("func excludeProperties", () => {
     expect(UTILS.Object.excludeProperties({ 1: "123", 2: "234", 3: "345" }, [2, 3])).toMatchObject({
       1: "123",
     });
+  });
+});
+
+describe("func copy", () => {
+  test("test number/boolean/string", () => {
+    expect(UTILS.Object.copy(1)).toBe(1);
+    expect(UTILS.Object.copy(true)).toBe(true);
+    expect(UTILS.Object.copy("string")).toBe("string");
+  });
+  
+  test("test simple object", () => {
+    const source = {
+      a: 1,
+      b: true,
+      c: "string",
+    };
+    const sourceStr = JSON.stringify(source);
+    const target = UTILS.Object.copy(source);
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.a = 2;
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.b = false;
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.c = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+  });
+
+  test("test complex object", () => {
+    const source = {
+      a: 1,
+      b: true,
+      c: "string",
+      d: { a: 1, b: true, c: "string", d: { a: 1, b: true, c: "string" }, e: [1, true, "string"] },
+      e: [1, true, "string", { a: 1, b: true, c: "string" }, [1, true, "string"]],
+    };
+    const sourceStr = JSON.stringify(source);
+    const target = UTILS.Object.copy(source);
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.a = 2;
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.b = false;
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.c = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.d.a = 2;
+    source.d.b = false;
+    source.d.c = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.d.d.a = 2;
+    source.d.d.b = false;
+    source.d.d.c = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.d.d = { a: 1, b: true, c: "string" };
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.d.e[0] = 2;
+    source.d.e[1] = false;
+    source.d.e[2] = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.d.e = [1, true, "string"];
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.e[0] = 2;
+    source.e[1] = false;
+    source.e[2] = "not a string";
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.e[3] = { a: 1, b: true, c: "string" };
+    expect(JSON.stringify(target)).toBe(sourceStr);
+    source.e[4] = [1, true, "string"];
+    expect(JSON.stringify(target)).toBe(sourceStr);
   });
 });
