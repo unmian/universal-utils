23d2ae8ff326c295e25eb97e407b2e59fb579965
Quarter <master@shiruihua.cn>
2023-01-16 17:08:30 +0800

    refactor: uri 序列化数据新增 fullPath 字段

--- packages/common/src/uri.ts
+++ packages/common/src/uri.ts
@@ -2,7 +2,7 @@
  * @Author: Quarter
  * @Date: 2023-01-16 14:47:36
  * @LastEditors: Quarter
- * @LastEditTime: 2023-01-16 16:30:51
+ * @LastEditTime: 2023-01-16 17:03:57
  * @FilePath: /universal-utils/packages/common/src/uri.ts
  * @Description: uri 模块
  */
@@ -11,15 +11,16 @@ import { copy } from "./object";
 
 // uri 结构
 interface URISerialization {
+  fullPath: string; // 完整路径
   hash: string | null; // hash 内容
-  hostname: string | null; // 主机名
+  host: string | null; // 主机名
   isSSL: boolean; // 是否是 SSL 协议
   isDefaultPort: boolean; // 是否是默认端口
-  uri: string; // 完整地址
-  pathname: string; // 路径地址
+  path: string; // 路径地址
   port: number; // 端口
   protocol: "http:" | "https:"; // 协议
   query: { [key: string]: string }; // uri 参数
+  uri: string; // 完整地址
 }
 
 // 协议头验证规则
@@ -37,15 +38,16 @@ const PARAM_REGEXP = /\?.+$/;
 
 // 初始值
 const URI_SERIALIZATION: URISerialization = {
+  fullPath: "/", // 完整路径
   hash: null, // hash 内容
-  hostname: null, // 主机名
+  host: null, // 主机名
   isSSL: false, // 是否是 SSL 协议
   isDefaultPort: true, // 是否是默认端口
-  uri: "/", // 完整地址
-  pathname: "/", // 路径地址
+  path: "/", // 路径地址
   port: 80, // 端口
   protocol: "http:", // 协议
   query: {}, // uri 参数
+  uri: "/", // 完整地址
 };
 
 /**
@@ -86,7 +88,7 @@ export const parse = (uri: string): URISerialization => {
     temp = temp.replace(HASH_REGEXP, "");
     const slashIndex = temp.indexOf("/");
     if (slashIndex > -1) {
-      result.pathname = temp.substring(slashIndex);
+      result.path = temp.substring(slashIndex);
     }
     // port 处理
     temp = temp.replace(PATH_REGEXP, "");
@@ -100,13 +102,14 @@ export const parse = (uri: string): URISerialization => {
     }
     // host 处理
     temp = temp.replace(PORT_REGEXP, "");
-    result.hostname = temp || null;
+    result.host = temp || null;
   }
   // 判定值处理
   result.isSSL = result.protocol === "https:";
   result.isDefaultPort =
     (result.protocol === "http:" && result.port === 80) ||
     (result.protocol === "https:" && result.port === 443);
+  result.fullPath = stringify(result, true);
   result.uri = stringify(result);
   return result;
 };
@@ -114,27 +117,31 @@ export const parse = (uri: string): URISerialization => {
 /**
  * @description: 将序列化数据转换为字符串
  * @param {URISerialization} uri 序列化数据
+ * @param {boolean} withoutHost 是否移除主机地址
  * @return {string}
  */
-export const stringify = (uri: URISerialization): string => {
+export const stringify = (
+  uri: Pick<URISerialization, "hash" | "host" | "path" | "port" | "protocol" | "query">,
+  withoutHost = false,
+): string => {
   let str = "";
   // host 处理
-  if (typeof uri.hostname === "string" && uri.hostname.length > 0) {
+  if (withoutHost === false && typeof uri.host === "string" && uri.host.length > 0) {
     if (
       (uri.protocol === "http:" && uri.port === 80) ||
       (uri.protocol === "https:" && uri.port === 443)
     ) {
-      str += `${uri.protocol}//${uri.hostname}`;
+      str += `${uri.protocol}//${uri.host}`;
     } else {
-      str += `${uri.protocol}//${uri.hostname}:${uri.port}`;
+      str += `${uri.protocol}//${uri.host}:${uri.port}`;
     }
   }
   // path 处理
-  if (uri.pathname.length > 0) {
+  if (uri.path.length > 0) {
     if (str.length === 0) {
-      str += uri.pathname;
-    } else if (uri.pathname !== "/") {
-      str += uri.pathname;
+      str += uri.path;
+    } else if (uri.path !== "/") {
+      str += uri.path;
     }
   }
   // hash 处理
--- tests/uri.test.ts
+++ tests/uri.test.ts
@@ -2,7 +2,7 @@
  * @Author: Quarter
  * @Date: 2023-01-13 17:00:29
  * @LastEditors: Quarter
- * @LastEditTime: 2023-01-16 16:26:41
+ * @LastEditTime: 2023-01-16 17:04:41
  * @FilePath: /universal-utils/tests/uri.test.ts
  * @Description: uri 相关单元测试
  */
@@ -13,73 +13,79 @@ import { describe, expect, test } from "vitest";
 describe("func parse", () => {
   test("parse single type url", () => {
     expect(URI.parse("www.baidu.com")).toMatchObject({
+      fullPath: "/",
       hash: null, // hash 内容
-      hostname: "www.baidu.com", // 主机名
+      host: "www.baidu.com", // 主机名
       isSSL: false, // 是否是 SSL 协议
       isDefaultPort: true, // 是否是默认端口
-      uri: "http://www.baidu.com", // 完整地址
-      pathname: "/", // 路径地址
+      path: "/", // 路径地址
       port: 80, // 端口
       protocol: "http:", // 协议
       query: {}, // uri 参数
+      uri: "http://www.baidu.com", // 完整地址
     });
     expect(URI.parse("https://www.baidu.com")).toMatchObject({
+      fullPath: "/",
       hash: null, // hash 内容
-      hostname: "www.baidu.com", // 主机名
+      host: "www.baidu.com", // 主机名
       isSSL: true, // 是否是 SSL 协议
       isDefaultPort: true, // 是否是默认端口
-      uri: "https://www.baidu.com", // 完整地址
-      pathname: "/", // 路径地址
+      path: "/", // 路径地址
       port: 443, // 端口
       protocol: "https:", // 协议
       query: {}, // uri 参数
+      uri: "https://www.baidu.com", // 完整地址
     });
     expect(URI.parse("http://www.baidu.com:8080")).toMatchObject({
+      fullPath: "/",
       hash: null, // hash 内容
-      hostname: "www.baidu.com", // 主机名
+      host: "www.baidu.com", // 主机名
       isSSL: false, // 是否是 SSL 协议
       isDefaultPort: false, // 是否是默认端口
-      uri: "http://www.baidu.com:8080", // 完整地址
-      pathname: "/", // 路径地址
+      path: "/", // 路径地址
       port: 8080, // 端口
       protocol: "http:", // 协议
       query: {}, // uri 参数
+      uri: "http://www.baidu.com:8080", // 完整地址
     });
     expect(URI.parse("/test")).toMatchObject({
+      fullPath: "/test",
       hash: null, // hash 内容
-      hostname: null, // 主机名
+      host: null, // 主机名
       isSSL: false, // 是否是 SSL 协议
       isDefaultPort: true, // 是否是默认端口
-      uri: "/test", // 完整地址
-      pathname: "/test", // 路径地址
+      path: "/test", // 路径地址
       port: 80, // 端口
       protocol: "http:", // 协议
       query: {}, // uri 参数
+      uri: "/test", // 完整地址
     });
     expect(URI.parse("#test")).toMatchObject({
+      fullPath: "/#test",
       hash: "test", // hash 内容
-      hostname: null, // 主机名
+      host: null, // 主机名
       isSSL: false, // 是否是 SSL 协议
       isDefaultPort: true, // 是否是默认端口
       uri: "/#test", // 完整地址
-      pathname: "/", // 路径地址
+      path: "/", // 路径地址
       port: 80, // 端口
       protocol: "http:", // 协议
       query: {}, // uri 参数
     });
     expect(URI.parse("?name=test")).toMatchObject({
+      fullPath: "/?name=test",
       hash: null, // hash 内容
-      hostname: null, // 主机名
+      host: null, // 主机名
       isSSL: false, // 是否是 SSL 协议
       isDefaultPort: true, // 是否是默认端口
-      uri: "/?name=test", // 完整地址
-      pathname: "/", // 路径地址
+      path: "/", // 路径地址
       port: 80, // 端口
       protocol: "http:", // 协议
       query: {
         // uri 参数
         name: "test",
       },
+      uri: "/?name=test", // 完整地址
     });
   });
 
@@ -87,12 +93,12 @@ describe("func parse", () => {
     expect(
       URI.parse("https://www.baidu.com:4443/custom/path#/test/hash?test1=&test2=a"),
     ).toMatchObject({
+      fullPath: "/custom/path#/test/hash?test1=&test2=a",
       hash: "/test/hash", // hash 内容
-      hostname: "www.baidu.com", // 主机名
+      host: "www.baidu.com", // 主机名
       isSSL: true, // 是否是 SSL 协议
       isDefaultPort: false, // 是否是默认端口
-      uri: "https://www.baidu.com:4443/custom/path#/test/hash?test1=&test2=a", // 完整地址
-      pathname: "/custom/path", // 路径地址
+      path: "/custom/path", // 路径地址
       port: 4443, // 端口
       protocol: "https:", // 协议
       query: {
@@ -100,6 +106,7 @@ describe("func parse", () => {
         test1: "",
         test2: "a",
       },
+      uri: "https://www.baidu.com:4443/custom/path#/test/hash?test1=&test2=a", // 完整地址
     });
   });
 });
